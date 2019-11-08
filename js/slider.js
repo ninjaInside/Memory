93c7dd7351900235266af6b38f57ff9a2608d083
Кирилл <mr.glazki@mail.ru>
2019-11-08 20:12:19 +0900

    work is end

--- js/slider.js
+++ js/slider.js
@@ -1,36 +1,9 @@
-function cache(key, value) {
-
-	if (typeof(value) == 'undefined') {
-		return cache[key];
-	}
-
-	cache[key] = value;
-
-}
-
-function _id_(id) {
-
-	if (!cache(id)) {
-    cache(id, document.getElementById(id));
-  }
-
-	return cache(id);
-
-}
-
-let x = 0;
-
-let slider = setTimeout(function slide() {
-
-  if (x === -1500) {
-    x = 500;
-    _id_('pic').style.left = x + 'px';
-  }
-
-  x += -500;
-
-   _id_('pic').style.left = x + 'px';
-
-  slider = setTimeout(slide, 2000);
-
-}, 2000);
+$(document).ready(function() {
+    $('.slider').cycle({
+    fx: 'fade',
+    speed: 900, 
+    timeout: 1000,
+    random: 1,
+    pause: 1,
+  });
+});
