d0d34871a54b14e8641623ff2a47b68efa32dad3
artemowa <loskan2017@yandex.ru>
2019-12-27 00:55:35 +0900

    only source code html/css/js

--- posts/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-from .models import Veteran
-
-
-def posts(request):
-    veterans = Veteran.objects.all()
-    return render(request, 'posts/posts.html', {'veterans': veterans})
