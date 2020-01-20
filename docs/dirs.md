9b31d033a29b9e716f6c057978fcfb8b620fb81d
artemowa <loskan2017@yandex.ru>
2020-01-20 17:26:09 +0900

    return main memory package

---
+++ docs/dirs.md
@@ -0,0 +1,5 @@
+# Static folders paths
+
+These are a paths to folders with static files (like a JS, CSS, HTML)
+
+* **/memory/
---
+++ memory/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', RedirectView.as_view(url='home/')),
+    path('home/', include('homepage.urls')),
+    path('blog/', include('blog.urls')),
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
