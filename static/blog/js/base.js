a6242d3f3c89aec155d1dda3f21f754cf921d5c0
ninjaInside <mr.glazki@mail.ru>
2020-04-30 03:43:38 +0900

    fix simple bag's

--- static/blog/js/base.js
+++ static/blog/js/base.js
@@ -1,3 +1,30 @@
+let currentUser;
+	currentUser = $('.menu__btn').text()
+
 $(document).ready(() => {
-	$('.menu__btn').click(() => $('.menu__list').toggleClass('menu__list_show'))
+
+	$('.menu__btn').click((e) => {
+		$(e.currentTarget).children('img').toggleClass('menu__btn_close')
+
+		$('.menu__list').toggleClass('menu__list_show')
+	})
+	
+	adaptiveMenuBtn(window);
+
+	$(window).resize((e) => {
+		adaptiveMenuBtn(e.target)
+	})
 })
+
+
+function adaptiveMenuBtn(target) {
+	if ($(target).width() <= 1030) {
+
+		$('.menu__btn').html('<img src="#" alt="">')
+	
+	} else if ($(target).width() >= 1030) {
+
+		$('.menu__btn').html(currentUser)
+
+	}
+}
